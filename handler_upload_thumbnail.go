dfa54fd5410a7e9a8528548ee3e5fcfea825111f
h4r5h1l <h4r5h1l@gmail.com>
2026-03-11 07:33:42 -0400

    ch 1 l6 encoding

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
@@ -72,8 +73,10 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		mediaType: mediaType,
 	}
 	videoThumbnails[videoID] = thumbnail
-	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
-	video.ThumbnailURL = &url
+
+	url := base64.StdEncoding.EncodeToString([]byte(data))
+	dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, url)
+	video.ThumbnailURL = &dataURL
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't update video with thumbnail URL", err)
